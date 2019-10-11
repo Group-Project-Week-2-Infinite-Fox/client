8b31b846177e3006b2510c0d284dcb6690c03eff
Ricky <audrickong.ao@gmail.com>
2019-10-11 09:00:58 +0700

    Nav

---
+++ scripts/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Nav></Nav>
+    </div>
+</template>
+
+<script>
+
+import Nav from './components/Nav'
+
+export default {
+    data() {
+        return {
+            message: 'Hello world',
+        }
+    },
+    components: {
+        Nav
+    }
+}
+</script>
+
+<style scoped>
+</style>
